760cbab15598cff9ac6721d90555d8e0453e04a7
Mahyar Abbas Pour <mahyar.abaspour@gmail.com>
2016-03-05 23:27:21 +0330

    Add documentation

--- src/ed.h
+++ src/ed.h
@@ -5,54 +5,76 @@
 #include <limits.h>
 #include <assert.h>
 
+/* Constants */
 #define BUFFSIZE 128
 
+/* Data structures */
+
 typedef struct Range {
+	/* Beginning line */
 	int beg;
+	/* Ending line */
 	int end;
 } Range;
 
 typedef struct Command {
+	/* The range of lines the functions act on */
 	Range range;
+	/* A one-character command */
 	char cmd;
+	/* Contains the arguments passed to the functions */
 	char *arg;
 } Command;
 
 typedef struct Line {
+	/* Holds the actual text */
 	char *text;
+	/* The line number */
 	int line_no;
 	struct Line *next;
 	struct Line *prev;
 } Line;
 
 typedef struct Buffer {
+	/* The path of the associated file */
 	char path[PATH_MAX];
 	Line *first_line;
 	Line *last_line;
 	Line *cur_line;
 } Buffer;
 
+/* Declaration of globals */
+
+/* Points to the single global buffer */
 extern Buffer *curbuf;
 
+/* Typedefs */
 typedef void (*function)(Command*);
 
+/* Function prototypes */
+
+/* Utils.c */
 void strstrip(char *str);
 char *charalloc(size_t len);
 char *charrealloc(char *buffer, size_t len);
+void clrstr(char *str);
+
+/* file.c */
 Buffer *new_buffer(const char *path);
 Line *new_line(const char *text, int line_no);
 void push_back_line(Buffer *buffer, Line *line);
 void push_front_line(Buffer *buffer, Line *line);
 void insert_line(Buffer *buffer, Line *line);
+char *read_line(FILE *fs);
+void write_buffer(const char *path);
+
+/* cmds.c */
+Command *new_cmd();
+void delete_cmd(Command *command);
 Command *parse_cmd(char *cmdstr);
 function find_function(Command *command);
 void append(Command *command);
 void quit(Command *command);
-void unknown(Command *command);
-char *read_line(FILE *fs);
-void write_buffer(const char *path);
 void write_out(Command *command);
-void delete_cmd(Command *command);
-void clrstr(char *str);
-Command *new_cmd();
+void unknown(Command *command);
 #endif
